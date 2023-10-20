e9e705fe171a035dc7e15e688bc4c5813726c73e
гном убийца 150 <  150@INBOOK_X2_PLUS>
2023-10-20 16:44:18 +0300

    Добавьте файлы проекта.

---
+++ Pod_sale_reznya/Pod.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pod_sale_reznya
+{
+    internal class Pod
+    {
+        public static Dictionary<string, int> Names = new Dictionary<string, int>()
+        {
+            {"Aegis",200 },
+            {"Charon",400 },
+            {"Pasito",300 }
+        };
+        public static Dictionary<string, int> Models = new Dictionary<string, int>()
+        {
+            {"B60", 1500 },
+            {"Baby plus",800 },
+            {"второй",2000 }
+        };
+        public static Dictionary<string, int> Counts = new Dictionary<string, int>()
+        {
+            {"infinity", 2000 },
+            {"20000", 1250 },
+            {"12000", 800 }
+        };
+        public static Dictionary<string, int> Equipments = new Dictionary<string, int>()
+        {
+            {"Базовая", 500 },
+            {"Продвинутая", 800 },
+            {"Полная", 1200 }
+        };
+        public static Dictionary<string, int> Auto_tyagi_ = new Dictionary<string, int>()
+        {
+            {"Где я", 0 },
+            {"Автотяги", 300 },
+            {"От кнопки", 200 }
+        };
+        public static void pricelist(string title, Dictionary<string, int> dict, out Dictionary<string, int> tmp_dict, out string tmp_title_for_pod_menu)
+        {
+            Console.Clear();
+            Console.WriteLine("Выберите " + title);
+            foreach (var i in dict.Keys)
+            {
+                Console.WriteLine($"  {i}  -  {dict[i]}");
+            }
+            tmp_dict = dict;
+            tmp_title_for_pod_menu = title;
+        }
+        public static void categories(string result = "")
+        {
+            Console.Clear();
+            Console.WriteLine($"Выберите пункт, меню, подходящего под ваш Под, который Вы хотите заказать или нажмите пробел, чтобы завершить заказ:\n  Название\n  Модель\n  Количество затяг\n  Комплектация\n  Затяжки\nВаш под:\n{result}");
+        }
+    }
+}
---
+++ Pod_sale_reznya/Program.cs
@@ -0,0 +1,88 @@
+﻿using Pod_sale_reznya;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Pod_sale_reznya
+{
+    internal class Program
+    {
+        static int amount = 0;
+        static string result = "";
+        static string title = "";
+        private static void Main()
+        {
+            Dictionary<string, int> tmp = new Dictionary<string, int>();
+            Pod.categories(result == "" ? null : result);
+            try
+            {
+                int res = strelki(1, 5);
+                switch (res)
+                {
+                    case 1:
+                        Pod.pricelist("Название", Pod.Names, out tmp, out title);
+                        break;
+                    case 2:
+                        Pod.pricelist("Модель", Pod.Models, out tmp, out title);
+                        break;
+                    case 3:
+                        Pod.pricelist("Количество затяг", Pod.Counts, out tmp, out title);
+                        break;
+                    case 4:
+                        Pod.pricelist("Комплектация", Pod.Equipments, out tmp, out title);
+                        break;
+                    case 5:
+                        Pod.pricelist("Затяжки", Pod.Auto_tyagi_, out tmp, out title);
+                        break;
+                }
+                int new_res = strelki(1, 3);
+                result += $"{title}: {tmp.Keys.ToArray()[new_res-1]} -  {tmp[tmp.Keys.ToArray()[new_res - 1]]}\n";
+                amount += tmp[tmp.Keys.ToArray()[new_res-1]];
+                Main();
+            }
+            catch { Main(); }
+        }
+        public static int strelki(int start, int end)
+        {
+            ConsoleKeyInfo point;
+            int pos = start;
+            Console.SetCursorPosition(0, pos);
+            Console.Write("->");
+            do
+            {
+                point = Console.ReadKey(true);
+                Console.SetCursorPosition(0, pos);
+                Console.Write("  ");
+                if (point.Key == ConsoleKey.UpArrow)
+                {
+                    pos--;
+                    if (pos == start - 1) pos = end;
+                }
+                else if (point.Key == ConsoleKey.DownArrow)
+                {
+                    pos++;
+                    if (pos > end) pos = 1;
+                }
+
+                Console.SetCursorPosition(0, pos);
+                Console.Write("->");
+            } while (point.Key != ConsoleKey.Enter && point.Key != ConsoleKey.Spacebar);
+            if (point.Key == ConsoleKey.Spacebar)
+            {
+                work_for_file("result.txt", amount);
+                return 0;
+            }
+            else
+                return pos;
+        }
+        static void work_for_file(string path, int Amount)
+        {
+            result += $"\n\tИтоговый чек: {Amount}\n";
+            if (!File.Exists(path))
+                File.WriteAllText(path, "");
+            File.AppendAllText(path, result);
+            amount = 0;
+            result = "";
+            Main();
+        }
+    }
+}
